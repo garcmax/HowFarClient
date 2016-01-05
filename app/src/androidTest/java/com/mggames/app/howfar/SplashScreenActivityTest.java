69c40961668a2a20e704f90f5ca1e4a9d8496a52
niwamaxou <niwamaxou@gmail.com>
2016-01-05 15:10:11 +0100

    testing splashScreenActivity

--- app/src/androidTest/java/com/mggames/app/howfar/SplashScreenActivityTest.java
+++ app/src/androidTest/java/com/mggames/app/howfar/SplashScreenActivityTest.java
@@ -1,7 +1,11 @@
 package com.mggames.app.howfar;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
-import android.test.AndroidTestCase;
+import android.test.TouchUtils;
+import android.widget.Button;
 
 /**
  * Created by maxgarcia on 04/01/2016.
@@ -12,13 +16,36 @@ public class SplashScreenActivityTest extends ActivityInstrumentationTestCase2<S
         super(SplashScreenActivity.class);
     }
 
-    public void testActivityExists() {
+     public void testLoginButton() {
+
+        Instrumentation.ActivityMonitor activityMonitor = getInstrumentation().addMonitor(LoginActivity.class.getName(), null, false);
+
         SplashScreenActivity activity = getActivity();
-        assertNotNull(activity);
+        final Button loginButton = (Button) activity.findViewById(R.id.login_button);
+        TouchUtils.clickView(this, loginButton);
+
+        Activity nextActivity = getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 5000);
+        assertNotNull(nextActivity);
+        Intent myIntent = nextActivity.getIntent();
+        assertNotNull(myIntent);
+        final boolean isLogin = myIntent.getBooleanExtra(nextActivity.getString(R.string.is_login), true);
+        assertEquals(true, isLogin);
+        nextActivity .finish();
     }
 
-    public void testLoginButton() {
+    public void testRegisterButton() {
+        Instrumentation.ActivityMonitor activityMonitor = getInstrumentation().addMonitor(LoginActivity.class.getName(), null, false);
+
         SplashScreenActivity activity = getActivity();
-        
+        final Button registerButton = (Button) activity.findViewById(R.id.register_button);
+        TouchUtils.clickView(this, registerButton);
+
+        Activity nextActivity = getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 5000);
+        assertNotNull(nextActivity);
+        Intent myIntent = nextActivity.getIntent();
+        assertNotNull(myIntent);
+        final boolean isLogin = myIntent.getBooleanExtra(nextActivity.getString(R.string.is_login), true);
+        assertEquals(false, isLogin);
+        nextActivity .finish();
     }
 }
